a133aa60954f3266e7077c12d4c388780582f7a3
Swiga Bahati <bahatijacklee8@gmail.com>
2024-11-05 14:11:40 +0300

    third commit

--- api-management/server.js
+++ api-management/server.js
@@ -2,7 +2,9 @@ import express from 'express';
 
 const app= express();
 
-app.get("/",(req,res))
+app.get("/products",(req,res) =>{
+    res.send("Server is ready");
+})
 
 app.listen(5000, () => {
     console.log('server started at https://localhost:5000')
