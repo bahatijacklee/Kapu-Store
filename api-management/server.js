302e5821d24da9408573f46c43c008922475b7ca
Swiga Bahati <bahatijacklee8@gmail.com>
2024-11-07 14:30:26 +0300

    api last commit

--- api-management/server.js
+++ api-management/server.js
@@ -1,12 +1,20 @@
 import express from 'express';
+import dotenv from 'dotenv';
+//import { connectDB, connection } from './config/db.js';
+import { connectDB } from './config/db.js';
+
+import productRoutes from "./routes/product.route.js";
+
+dotenv.config();
 
 const app= express();
+const PORT = process.env.PORT || 5000;
+app.use(express.json()); 
 
-app.get("/products",(req,res) =>{
-    res.send("Server is ready");
-})
+app.use("/api/products", productRoutes);
 
-app.listen(5000, () => {
-    console.log('server started at https://localhost:5000')
+app.listen(PORT, () => {
+    connectDB();
+    console.log('server started at https://localhost:' + PORT);
 
-})
+});
